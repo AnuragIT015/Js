c9f4fcac3f986569f2ee3c9e516d95ba69a3d9d0
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-09-03 09:31:00 +0000

    Add variables.js and remove test2.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456; // integer
+let accountEmail ="anuu9893@gmail.com"
+var accountPassword = "12345";
+accountCity = "Bangalore"; // global variable
+//accountId = 12;
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
