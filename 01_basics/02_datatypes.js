452e819576c91441f28f4d217c95c2b739f05fff
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-09-03 09:18:21 +0000

    Add or update 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs not browser
+
+
+let name = "Anurag" //String
+let age = 19
+let isLoggedIn = false
+//number - 2 to power 53
+//bigint
+//string - '',"",``
+//boolean - true/false
+//null - standalone value
+//undefined - variable value not assigned
+//symbol - unique value
+
+//object - key value pairs
+//array - ordered list of values
+
+console.log(typeof isLoggedIn);
+console.log(typeof age);
+console.log(typeof name);
