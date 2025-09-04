b3441cc91cd379c902a42cec485b3a6bb70bfc33
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-09-04 04:21:11 +0000

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,8 @@
+/*
+// **************** Conversion of datatypes ****************
+
+// conversion operation
+
 let score = "33abc"
 console.log(typeof score) // string
 
@@ -17,11 +22,30 @@ console.log(typeof booleanIsLoggedIn) // boolean
 // 1 => true; 0 => false
 // "abc" => true; "" => false
 
-let someNumber = 33;
+let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber) // string
 // 33 => "33"
 // true => "true"; false =  > "false" 
 // null => "null"; undefined => "undefined"
+*/
+// **************** Operations ****************
 
-// Type coercion
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
+console.log(-negValue) // 3
+
+let str = "hello "
+let str2 = "world "
+let str3 = str + str2
+console.log(str3) // hello world
+
+console.log("1" + 2) // "12"
+console.log(1 + "2") // "12"
+console.log("1" + 2 + 2) // "122"
+console.log(1 + 2 + "2") // "32"
+
+console.log(3 - "1") // 2
+console.log("3" - 1) // 2
+console.log("3" - "1") // 2
