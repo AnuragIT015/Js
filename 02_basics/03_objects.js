48ef21f599744cd591973647edf61a42abe56509
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-09-11 19:20:19 +0000

    Update 03_objects.js for improved object printing

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -8,7 +8,7 @@ const JsUser ={
     name: "Anurag",
     "full name": "Anurag Gupta",
     mySym : "mykey1",// now its datatype is String
-    [mySym]: myValue1, // now its datatype is Symbol
+    [mySym]: "myValue1", // now its datatype is Symbol
     age: 24,
     location: "Indore",
     email:"anuu9893@gmail.com",
@@ -28,5 +28,23 @@ console.log(JsUser[mySym]) // bracket notation
 
 console.log(typeof JsUser[mySym]) // datatype is Symbol
 
-JsUser.email = "anuu9994@gmail.com"
+JsUser.email = "anuu9994@gmail.com" // update the email
 
+Object.freeze(JsUser) // it will not allow to change any data of object 
+JsUser.email = "anuu1111@gmail.com" // it will not change the email
+
+console.log(JsUser); // name: 'Anurag', 'full name': 'Anurag Gupta', mySym: 'mykey1', [Symbol(key1)]: 'myValue1', age: 24, location: 'Indore', email: '
+// imp [Symbol(key1)]: 'myValue1' actually a symbol key
+
+JsUser.greeting = function(){
+    console.log("Hello JsUser");
+}
+ // adding function in object
+
+ JsUser.greeting2 = function(){
+    console.log(`Hello ${this.name} `); // this is used to access the custom property of an object
+ }
+
+ console.log(JsUser.greeting()); // Hello JsUser
+
+ console.log(JsUser.greeting2()); // Hello Anurag
