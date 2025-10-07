b5dee8587ce4d7e58ef915bafdb410265c0456a6
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-10-07 17:32:42 +0000

    Add or update two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+let index = 0;
+while(index <=10){
+    console.log(`value of index is: ${index}`);
+    index+=2;
+} // prints even numbers from 0 to 10
+
+let myArray = ['flash', 'batman', 'superman', 'aquaman'];
+
+let arr=0;
+while(arr<myArray.length){
+    console.log(myArray[arr]);
+    arr++;
+} // prints all the elements of the array
+
+// do while loop
+let score =0;
+
+do{
+console.log(`score is : ${score}`);
+score++;
+}
+while(
+    score<=10
+) // prints score from 0 to 10
+
+
