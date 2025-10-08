417373411d855ce2045f55f9606062ac5b352806
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-10-08 15:25:34 +0000

    Add or update four.js

---
+++ 05_iterations/four.js
@@ -0,0 +1,23 @@
+const myObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'swift by apple'
+}
+for(const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+//js shortcut is for Javascript
+//cpp shortcut is for C++
+//rb shortcut is for Ruby
+//swift shortcut is for swift by apple
+
+const myArray = ['flash', 'batman', 'superman', 'aquaman'];
+for(const index in myArray){
+    console.log(key)
+} //prints index of each element in the array
+
+for(const index in myArray){
+    console.log(myArray[key])
+} //prints each element in the array
+
