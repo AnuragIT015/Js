48f938efa80afd58fb1357292b28b5e04537071a
AnuragIT015 <88150105+AnuragIT015@users.noreply.github.com>
2025-12-30 18:46:16 +0000

    promises

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -46,6 +46,55 @@ const promiseFour = new Promise(function(resolve, reject) {
 const username = promiseFour.then((user) => {
     console.log(user);
     return user.username;
-})
+}).then((username)=> {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("Promise is either resolved or rejected"));
 
-console.log(username); // Promise { <pending> }
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error)
+            resolve({username: "javascript" , password: "123"});
+        else
+            reject("Error: JS went wrong");
+    },1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response =await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+async function getAllUsers(){
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data =await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+getAllUsers();
+
+// same in .then().catch()
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response) => {
+    return response.json();
+})
+.then(() => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log("Error:", error);
+})
